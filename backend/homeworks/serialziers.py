59cfad6f09fdd80e2156476d4c1d2eeb122bf4e2
Bekarys <bekarys20050812@gmail.com>
2025-07-22 23:52:04 +0500

    homework views add

--- backend/homeworks/serialziers.py
+++ backend/homeworks/serialziers.py
@@ -1,4 +1,13 @@
 from rest_framework import serializers
-
+from .models import HomeworkClassroomAssign
 class HomeworkCreateSerializer(serializers.ModelSerializer):
-    pass
+    class Meta:
+        fields = ['title', 'description', 'due_date', 'attachment', 'is_optional']
+        read_only_fields = ['assigned_by']
+
+    def create(self, validated_data):
+        tutor = self.context['tutor'] 
+        return HomeworkClassroomAssign.objects.create(
+            assigned_by=tutor,
+            **validated_data
+        )
--- backend/homeworks/views.py
+++ backend/homeworks/views.py
@@ -3,19 +3,19 @@ from rest_framework.views import APIView
 from rest_framework import permissions
 from tutors.models import Tutor
 from rest_framework.exceptions import NotFound
+from rest_framework.generics import CreateAPIView
 # from django.contrib.auth.models import User
 # Create your views here.
 
 
-class HomeworkCreateView(APIView):
+class HomeworkCreateView(CreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     
-    def post(self, request):
-        user = self.request.user
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
         try:
-            tutor = Tutor.objects.get(user = user)
+            tutor = Tutor.objects.get(user=self.request.user)
         except Tutor.DoesNotExist:
-            raise NotFound("Tutor not found for this user.")
-        
-        pass
-    
+            raise NotFound("not found tutor")
+        context['tutor'] = tutor
+        return context
